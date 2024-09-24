0d996f27b5bc2b68e6ac89876ecbf7d540c85a9b
aditya-1123 <gaikwadaditya1123@gmail.com>
2024-09-24 07:53:05 +0530

    Your commit message

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');                   
 require('dotenv').config();
 
 // Define the mongoDB connection URL
-// const mongoURL = 'process.' // 'hotels' is the database name
+// const mongoURL = process.env.MONGODB_URL_LOCAL // 'hotels' is the database name
 const mongoURL = process.env.MONGODB_URI;
 console.log('MongoDB URL:', mongoURL);
 // Set up MongoDB connection 
--- server.js
+++ server.js
@@ -18,12 +18,24 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
 
 const boadyParser = require('body-parser');
 app.use(boadyParser.json());
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
+// Middleware Function 
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+
+app.use(logRequest);
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', {session: false});
+
+app.get('/', (req, res) => {    
     res.send("Welcome to our Hotel!");
 })
 
@@ -32,10 +44,9 @@ const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes');
 
 // Use the routers
-app.use('/person', personRoutes);
+app.use('/person', localAuthMiddleware, personRoutes);
 app.use('/menu', menuItemRoutes);
 
 app.listen(PORT, () => {
     console.log("listening on port 8000");  
 });
-
